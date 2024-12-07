c1861c8a9a4431b46bc1fbd55558f6899639d665
Kenneth Gregorio Perez <148478983+PolongDev@users.noreply.github.com>
2024-12-07 23:50:17 +0800

    Update __init__.py

--- AnimeScraper/__init__.py
+++ AnimeScraper/__init__.py
@@ -4,8 +4,7 @@ from .SyncScraper import SyncKunYu
 __all__ = ["KunYu", "SyncKunYu"]
 
 # Package metadata
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 __author__ = "Kenneth Perez"
 __email__ = "kennethgregorioperez@gmail.com"
 __license__ = "GPL-V3.0"
-
